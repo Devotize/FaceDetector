5cb77eeeaab10b884915d276030e49b71306dba1
sychev <s.denis_2001@mail.ru>
2021-12-18 15:44:22 +0300

    changed novice_flg type from string to int

--- app/src/main/java/com/sychev/facedetector/utils/Constants.kt
+++ app/src/main/java/com/sychev/facedetector/utils/Constants.kt
@@ -5,7 +5,7 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 
 const val TAG = "AppDebug"
-const val MIN_SCORE = 0.6f
+const val MIN_SCORE = 0.4f
 
 fun String.decodeToBitmap(): Bitmap {
     val imageBytes = Base64.decode(this, 0)
